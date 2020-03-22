73f39b3443ba9bf66d16d639173bb64e65d69126
HumHub Translations <info@humhub.com>
2020-03-22 19:17:08 +0100

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/hr/base.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    '<strong>Jitsi</strong> module configuration' => '',
+    'Close' => '',
+    'Default is meet.jit.si without "https://" prefix.' => '',
+    'Default: Jitsi Meet' => '',
+    'Default: empty, useful for public Jitsi server' => '',
+    'Invite' => '',
+    'Join' => '',
+    'Name' => '',
+    'Open conference room' => '',
+    'Open in new window?' => '',
+    'Specify JWT token to enable authentication' => '',
+];
