1bfdd0c17df7e3f467d1f45f20353e066e5e9a25
HumHub Translations <info@humhub.com>
2020-03-28 12:57:26 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Jitsi</strong> module configuration' => '',
-    'Close' => '',
-    'Default is meet.jit.si without "https://" prefix.' => '',
-    'Default: Jitsi Meet' => '',
-    'Default: empty, useful for public Jitsi server' => '',
-    'Invite' => '',
-    'Join' => '',
-    'Name' => '',
-    'Open conference room' => '',
-    'Open in new window?' => '',
-    'Specify JWT token to enable authentication' => '',
-];
+return array (
+  '<strong>Jitsi</strong> module configuration' => '<strong>Jitsi</strong> Modul-Einrichtung',
+  'Close' => 'Schließen',
+  'Default is meet.jit.si without "https://" prefix.' => 'Standard ist meet.jit.si ohne "https://" prefix.',
+  'Default: Jitsi Meet' => 'Standard: Jitsi Meet',
+  'Default: empty, useful for public Jitsi server' => 'Standard: leer, verwendbar für den öffentlichen Jitsi-Server',
+  'Invite' => 'Einladen',
+  'Join' => 'Beitreten',
+  'Name' => 'Name',
+  'Open conference room' => 'Konferenzraum öffnen',
+  'Open in new window?' => 'In neuem Fenster öffnen?',
+  'Specify JWT token to enable authentication' => 'Definiere das JWT-Token um die Authentifizierung zu aktivieren',
+);
--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,15 +1,14 @@
 <?php
-
-return [
-    '<strong>Jitsi</strong> module configuration' => '',
-    'Close' => '',
-    'Default is meet.jit.si without "https://" prefix.' => '',
-    'Default: Jitsi Meet' => '',
-    'Default: empty, useful for public Jitsi server' => '',
-    'Invite' => '',
-    'Join' => '',
-    'Name' => '',
-    'Open conference room' => '',
-    'Open in new window?' => '',
-    'Specify JWT token to enable authentication' => '',
-];
+return array (
+  '<strong>Jitsi</strong> module configuration' => '<strong>Jitsi</strong> module configuratie',
+  'Close' => 'Sluit',
+  'Default is meet.jit.si without "https://" prefix.' => 'Standaard is meet.jit.si zonder voorvoegsel "https: //".',
+  'Default: Jitsi Meet' => 'Standaard: Jitsi Meet',
+  'Default: empty, useful for public Jitsi server' => 'Standaard: leeg, handig voor openbare Jitsi-server',
+  'Invite' => 'uitnodigen',
+  'Join' => 'Doe mee',
+  'Name' => 'Naam',
+  'Open conference room' => 'Open conferentieruimte',
+  'Open in new window?' => 'Openen in een nieuw venster?',
+  'Specify JWT token to enable authentication' => 'Geef een JWT-token op om verificatie in te schakelen',
+);
