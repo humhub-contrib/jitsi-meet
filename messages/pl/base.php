d50bec4304a54ff2aaefe2a576d1676982b29c74
HumHub Translations <info@humhub.com>
2020-05-03 22:45:24 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pl/base.php
+++ messages/pl/base.php
@@ -1,14 +1,14 @@
 <?php
 return array (
-  '<strong>Jitsi</strong> module configuration' => '',
+  '<strong>Jitsi</strong> module configuration' => 'Konfiguracja modułu <strong>Jitsi</strong>',
   'Close' => 'Zamknij',
-  'Default is meet.jit.si without "https://" prefix.' => '',
-  'Default: Jitsi Meet' => '',
-  'Default: empty, useful for public Jitsi server' => '',
+  'Default is meet.jit.si without "https://" prefix.' => 'Domyślne to meet.jit.si bez prefixu "https://"',
+  'Default: Jitsi Meet' => 'Domyślnie: Jitsi Meet',
+  'Default: empty, useful for public Jitsi server' => 'Domyślnie: puste, przydatne dla publicznych serwerów jitsi',
   'Invite' => 'Zaproś',
-  'Join' => '',
+  'Join' => 'Dołącz',
   'Name' => 'Nazwa',
-  'Open conference room' => '',
-  'Open in new window?' => '',
-  'Specify JWT token to enable authentication' => '',
+  'Open conference room' => 'Otwórz pokój konferencji',
+  'Open in new window?' => 'Otworzyć w nowym oknie?',
+  'Specify JWT token to enable authentication' => 'Podaj token JWT aby umożliwić uwierzytelnianie',
 );
