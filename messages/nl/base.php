43ec7ac6bb65f35e9fa337171f8f00eb50f7eed2
HumHub Translations <info@humhub.com>
2020-09-11 18:02:25 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,16 +1,15 @@
 <?php
-
-return [
-    '<strong>Jitsi</strong> module configuration' => '<strong>Jitsi</strong> module configuratie',
-    'Close' => 'Sluit',
-    'Default is meet.jit.si without "https://" prefix.' => 'Standaard is meet.jit.si zonder voorvoegsel "https: //".',
-    'Default: Jitsi Meet' => 'Standaard: Jitsi Meet',
-    'Default: empty, useful for public Jitsi server' => 'Standaard: leeg, handig voor openbare Jitsi-server',
-    'Invite' => 'uitnodigen',
-    'Join' => 'Doe mee',
-    'Name' => 'Naam',
-    'Open conference room' => 'Open conferentieruimte',
-    'Open in new window?' => 'Openen in een nieuw venster?',
-    'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
-    'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
-];
+return array (
+  '<strong>Jitsi</strong> module configuration' => '<strong>Jitsi</strong> module configuratie',
+  'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => 'Applicatie-ID voor een privé Jitsi-server om een JWT-token te genereren voor authenticatie. Is standaard: leeg: geen JWT-tokenverificatie gebruiken.',
+  'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => 'Toepassingsgeheim delen met een privé Jitsi-server om JWT-token voor verificatie te ondertekenen . Is sstandaard: leeg. Alleen nodig als een JWT-token moet worden gegenereerd.',
+  'Close' => 'Sluit',
+  'Default is meet.jit.si without "https://" prefix.' => 'Standaard is meet.jit.si zonder voorvoegsel "https: //".',
+  'Default: Jitsi Meet' => 'Standaard: Jitsi Meet',
+  'Default: empty, useful for public Jitsi server' => 'Standaard: leeg, handig voor openbare Jitsi-server',
+  'Invite' => 'uitnodigen',
+  'Join' => 'Doe mee',
+  'Name' => 'Naam',
+  'Open conference room' => 'Open conferentieruimte',
+  'Open in new window?' => 'Openen in een nieuw venster?',
+);
