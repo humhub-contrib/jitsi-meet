bcb0cc9147073a4ca3a2063488f8735dc2dc4138
Lucas Bartholemy <luke-@users.noreply.github.com>
2020-09-16 10:21:59 +0200

    Update base.php

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 return array (
   '<strong>Jitsi</strong> module configuration' => 'Configuración del módulo <strong>Jitsi</strong>',
   'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => 'ID de aplicación compartida con un servidor Jitsi privado que se utiliza para generar un token JWT para la autenticación. Valor predeterminado: vacío, no se utilizará la autenticación de token JWT.',
@@ -14,21 +13,3 @@ return array (
   'Open conference room' => 'Abrir conferencia',
   'Open in new window?' => 'Abrir en una nueva ventana?',
 );
-=======
-
-return [
-    '<strong>Jitsi</strong> module configuration' => 'Configuración del módulo <strong>Jitsi</strong>',
-    'Close' => 'Cerrar',
-    'Default is meet.jit.si without "https://" prefix.' => 'Por defecto es <strong>meet.jit.si</strong> sin el prefijo "https://"',
-    'Default: Jitsi Meet' => 'Por defecto: Jitsi Meet',
-    'Default: empty, useful for public Jitsi server' => 'Por defecto: vacío, útil para servidor público de Jitsi',
-    'Invite' => 'Invitar',
-    'Join' => 'Unirse',
-    'Name' => 'Nombre',
-    'Open conference room' => 'Abrir conferencia',
-    'Open in new window?' => 'Abrir en una nueva ventana?',
-    'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
-    'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
-    'Enable JWT Authentication' => '',
-];
->>>>>>> c49610a4d109d5534c51fe93ab456c863d4a399c
