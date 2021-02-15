96f70ec00a2a739a12e63d93d86d64998e95d07b
HumHub Translations <info@humhub.com>
2021-02-15 18:24:04 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,17 +1,16 @@
 <?php
-
-return [
-    'Close' => 'Fechar',
-    'Name' => 'Nome',
-    '<strong>Jitsi</strong> module configuration' => '',
-    'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
-    'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
-    'Default is meet.jit.si without "https://" prefix.' => '',
-    'Default: Jitsi Meet' => '',
-    'Default: empty, useful for public Jitsi server' => '',
-    'Invite' => '',
-    'Join' => '',
-    'Open conference room' => '',
-    'Open in new window?' => '',
-    'Enable JWT Authentication' => '',
-];
+return array (
+  '<strong>Jitsi</strong> module configuration' => '',
+  'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
+  'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
+  'Close' => 'Fechar',
+  'Default is meet.jit.si without "https://" prefix.' => '',
+  'Default: Jitsi Meet' => '',
+  'Default: empty, useful for public Jitsi server' => '',
+  'Enable JWT Authentication' => '',
+  'Invite' => 'Convidar',
+  'Join' => '',
+  'Name' => 'Nome',
+  'Open conference room' => '',
+  'Open in new window?' => '',
+);
---
+++ messages/sq/base.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    '<strong>Jitsi</strong> module configuration' => '',
+    'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
+    'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
+    'Close' => '',
+    'Default is meet.jit.si without "https://" prefix.' => '',
+    'Default: Jitsi Meet' => '',
+    'Default: empty, useful for public Jitsi server' => '',
+    'Enable JWT Authentication' => '',
+    'Invite' => '',
+    'Join' => '',
+    'Name' => '',
+    'Open conference room' => '',
+    'Open in new window?' => '',
+];
