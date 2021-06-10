4a8d2e774298b429e5914f0132da40642fb6c949
HumHub Translations <info@humhub.com>
2021-06-10 20:13:49 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/base.php
+++ messages/th/base.php
@@ -1,17 +1,16 @@
 <?php
-
-return [
-    'Close' => 'ปิด',
-    '<strong>Jitsi</strong> module configuration' => '',
-    'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
-    'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
-    'Default is meet.jit.si without "https://" prefix.' => '',
-    'Default: Jitsi Meet' => '',
-    'Default: empty, useful for public Jitsi server' => '',
-    'Invite' => '',
-    'Join' => '',
-    'Name' => '',
-    'Open conference room' => '',
-    'Open in new window?' => '',
-    'Enable JWT Authentication' => '',
-];
+return array (
+  '<strong>Jitsi</strong> module configuration' => '<strong>จิ้ดสิ</strong> การกำหนดค่าโมดูล',
+  'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => 'ID แอปพลิเคชันที่แชร์กับเซิร์ฟเวอร์ Jitsi ส่วนตัวที่ใช้สร้างโทเค็น JWT สำหรับการตรวจสอบสิทธิ์ ค่าเริ่มต้น: ว่างเปล่า จะไม่มีการใช้การพิสูจน์ตัวตนโทเค็น JWT',
+  'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => 'ข้อมูลลับของแอปพลิเคชันที่แชร์กับเซิร์ฟเวอร์ Jitsi ส่วนตัวที่ใช้ในการลงนามโทเค็น JWT สำหรับการตรวจสอบสิทธิ์ ค่าเริ่มต้น: ว่างเปล่า จำเป็นหากควรสร้างโทเค็น JWT',
+  'Close' => 'ปิด',
+  'Default is meet.jit.si without "https://" prefix.' => 'ค่าเริ่มต้นคือ meet.jit.si โดยไม่มี "https://" นำหน้า',
+  'Default: Jitsi Meet' => 'ค่าเริ่มต้น: Jitsi Meet',
+  'Default: empty, useful for public Jitsi server' => 'ค่าเริ่มต้น: ว่างเปล่า มีประโยชน์สำหรับเซิร์ฟเวอร์ Jitsi สาธารณะ',
+  'Enable JWT Authentication' => 'เปิดใช้งานการตรวจสอบ JWT',
+  'Invite' => 'เชิญ',
+  'Join' => 'เข้าร่วม',
+  'Name' => 'ชื่อ',
+  'Open conference room' => 'เปิดห้องประชุม',
+  'Open in new window?' => 'เปิดหน้าต่างใหม่?',
+);
