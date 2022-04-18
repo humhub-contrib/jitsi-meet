f296e1325a20a6b800316ca6f101ede67155feb3
Yura Bakhtin <yurybakh@gmail.com>
2022-04-18 11:58:56 +0300

    Fix assets on updating of disabled module

--- assets/Assets.php
+++ assets/Assets.php
@@ -5,6 +5,7 @@ namespace humhubContrib\modules\jitsiMeet\assets;
 use humhubContrib\modules\jitsiMeet\Module;
 use Yii;
 use yii\web\AssetBundle;
+use yii\web\View;
 
 class Assets extends AssetBundle
 {
@@ -12,27 +13,28 @@ class Assets extends AssetBundle
     public $publishOptions = [
         'forceCopy' => true
     ];
-    public $css = [];
+
     public $jsOptions = [
-        'position' => \yii\web\View::POS_BEGIN
+        'position' => View::POS_BEGIN
     ];
 
     public function init()
     {
-        $this->js = [
-            $this->getJitsiApiJs(),
-            'humhub.jitsiMeet.js'
-        ];
-
+        $this->initJitsiApiJs();
         $this->sourcePath = dirname(__FILE__) . '/../resources';
         parent::init();
     }
 
-    private function getJitsiApiJs()
+    private function initJitsiApiJs()
     {
         /** @var Module $module */
         $module = Yii::$app->getModule('jitsi-meet');
-        return 'https://' . $module->getSettingsForm()->jitsiDomain . '/external_api.js';
+        if ($module instanceof Module) {
+            $this->js = [
+                'https://' . $module->getSettingsForm()->jitsiDomain . '/external_api.js',
+                'humhub.jitsiMeet.js'
+            ];
+        }
     }
 
 }
