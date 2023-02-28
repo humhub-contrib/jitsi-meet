d5f9b02f76a35404f047d2480eee770c2183d136
HumHub Translations <hello@humhub.com>
2023-02-28 07:37:59 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ca/base.php
+++ messages/ca/base.php
@@ -1,16 +1,15 @@
 <?php
-
-return [
-    'Close' => 'Tanca',
-    'Name' => 'Nom',
-    '<strong>Jitsi</strong> module configuration' => '',
-    'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
-    'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
-    'Default is meet.jit.si without "https://" prefix.' => '',
-    'Default: Jitsi Meet' => '',
-    'Default: empty, useful for public Jitsi server' => '',
-    'Enable JWT Authentication' => '',
-    'Join' => '',
-    'Open conference room' => '',
-    'Open in new window?' => '',
-];
+return array (
+  '<strong>Jitsi</strong> module configuration' => '',
+  'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => '',
+  'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => '',
+  'Close' => 'Tanca',
+  'Default is meet.jit.si without "https://" prefix.' => '',
+  'Default: Jitsi Meet' => '',
+  'Default: empty, useful for public Jitsi server' => '',
+  'Enable JWT Authentication' => '',
+  'Join' => 'Entrar',
+  'Name' => 'Nom',
+  'Open conference room' => '',
+  'Open in new window?' => '',
+);
