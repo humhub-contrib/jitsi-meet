5a8e0c59c0fb259b8ea6ea867021166a38d9b914
HumHub Translations <hello@humhub.com>
2024-02-23 19:29:20 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/base.php
+++ messages/fr/base.php
@@ -1,18 +1,17 @@
 <?php
-
-return [
-    '<strong>Jitsi</strong> module configuration' => 'Configuration du module <strong>Jitsi</strong>',
-    'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => 'ID d’application, partagée avec un serveur Jitsi privé servant à générer les jetons JTW pour l’authentification. Valeur par défaut : [vide], aucune authentification par jeton TWT.',
-    'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => 'Le secret de l’application, partagé avec un serveur Jitsi privé servant à signer les jetons JWT pour l’authentification. Valeur par défaut : [vide], la valeur est nécessaire si vous souhaitez générer un jeton JWT.',
-    'Close' => 'Fermer',
-    'Default is meet.jit.si without "https://" prefix.' => 'La valeur par défaut est meet.jit.si sans le préfixe "https://".',
-    'Default: Jitsi Meet' => 'Par défaut : Jitsi Meet',
-    'Default: empty, useful for public Jitsi server' => 'Par défaut : vide, utile sur le serveur public Jitsi',
-    'Enable JWT Authentication' => 'Activer l’authentification JWT',
-    'Join' => 'Rejoindre',
-    'Name' => 'Nom',
-    'Open conference room' => 'Ouvrir une salle de conférence',
-    'Open in new window?' => 'Ouvrir dans une nouvelle fenêtre ?',
-    'Can access Jitsi Meet' => '',
-    'Can access Jitsi Meet from main navigation.' => '',
-];
+return array (
+  '<strong>Jitsi</strong> module configuration' => 'Configuration du module <strong>Jitsi</strong>',
+  'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => 'ID d’application, partagée avec un serveur Jitsi privé servant à générer les jetons JTW pour l’authentification. Valeur par défaut : [vide], aucune authentification par jeton TWT.',
+  'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => 'Le secret de l’application, partagé avec un serveur Jitsi privé servant à signer les jetons JWT pour l’authentification. Valeur par défaut : [vide], la valeur est nécessaire si vous souhaitez générer un jeton JWT.',
+  'Can access Jitsi Meet' => 'Peut accéder à Jitsi Meet',
+  'Can access Jitsi Meet from main navigation.' => 'Peut accéder à Jitsi Meet depuis la navigation principale.',
+  'Close' => 'Fermer',
+  'Default is meet.jit.si without "https://" prefix.' => 'La valeur par défaut est meet.jit.si sans le préfixe "https://".',
+  'Default: Jitsi Meet' => 'Par défaut : Jitsi Meet',
+  'Default: empty, useful for public Jitsi server' => 'Par défaut : vide, utile sur le serveur public Jitsi',
+  'Enable JWT Authentication' => 'Activer l’authentification JWT',
+  'Join' => 'Rejoindre',
+  'Name' => 'Nom',
+  'Open conference room' => 'Ouvrir une salle de conférence',
+  'Open in new window?' => 'Ouvrir dans une nouvelle fenêtre ?',
+);
