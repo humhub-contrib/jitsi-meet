fd43e26ad64d2215b78332378b729fad55e776a0
HumHub Translations <hello@humhub.com>
2025-02-12 10:02:09 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt-BR/base.php
+++ messages/pt-BR/base.php
@@ -1,5 +1,5 @@
 <?php
-return array (
+return [
   '<strong>Jitsi</strong> module configuration' => 'Configuração do módulo <strong> Jitsi </strong>',
   'Application ID shared with a private Jitsi server used to generate JWT token for authentication. Default: empty, no JWT token authentication will be used.' => 'ID do aplicativo compartilhado com um servidor Jitsi privado usado para gerar token JWT para autenticação. Padrão: vazio, nenhuma autenticação de token JWT será usada.',
   'Application secret shared with a private Jitsi server used to sign JWT token for authentication. Default: empty, needed if JWT token should be generated.' => 'Segredo do aplicativo compartilhado com um servidor Jitsi privado usado para assinar o token JWT para autenticação. Padrão: vazio, necessário se o token JWT for gerado.',
@@ -10,8 +10,8 @@ return array (
   'Default: Jitsi Meet' => 'Padrão: Jitsi Meet',
   'Default: empty, useful for public Jitsi server' => 'Padrão: vazio, útil para o servidor público Jitsi',
   'Enable JWT Authentication' => 'Habilitar autenticação JWT',
-  'Join' => 'Junte-se',
+  'Join' => 'Participar',
   'Name' => 'Nome',
-  'Open conference room' => 'Sala de conferências aberta',
+  'Open conference room' => 'Abrir sala de conferência',
   'Open in new window?' => 'Abrir em uma nova janela?',
-);
+];
